3ce1869fb42664589a57cb8b87b5838052cbf238
sprayz <spray.jk2@gmail.com>
2013-06-13 12:06:40 +0200

    javaco en actividad instrucciones

--- src/proyecto/blocktris/ActividadInstrucciones.java
+++ src/proyecto/blocktris/ActividadInstrucciones.java
@@ -1,3 +1,6 @@
+/*
+ *  @author Pablo Morillas Lozano
+ */
 package proyecto.blocktris;
 
 import android.os.Bundle;
@@ -5,6 +8,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 
+
+/**
+ * ESta clase representa el diálogo de instrucciones.
+ */
 public class ActividadInstrucciones extends Activity {
 
 	
@@ -23,12 +30,19 @@ public class ActividadInstrucciones extends Activity {
 		c.startActivityForResult(i, 2);
 
 	}
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_actividad_instrucciones);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	
